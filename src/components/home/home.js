9911ca82a3894e70fb6a291677ebe3ef84fe2c67
idriss30 <idriss.cissoko@hotmail.com>
2022-08-31 19:13:00 -0400

    home component created

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import Collection from "../collection/collection";
+import HomeSlider from "../homeSlider/homeSlider";
+import MaterialCare from "../materials&Care/materialCareProgram";
+import NewsLetter from "../newsLetter/newsLetter";
+
+const Home = () => {
+  return (
+    <>
+      <HomeSlider />
+      <MaterialCare />
+      <Collection />
+      <NewsLetter />
+    </>
+  );
+};
+
+export default Home;
