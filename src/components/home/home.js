e61e144455af85264ab99d523e7b423e4f6cc782
idriss30 <idriss.cissoko@hotmail.com>
2022-09-06 23:03:59 -0400

    home component refactored, test written

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,17 +1,54 @@
 import Collection from "../collection/collection";
-import { HomeSlider } from "../homeSlider/homeSlider";
+import HomeSlider from "../homeSlider/homeSlider";
 import MaterialCare from "../materials&Care/materialCareProgram";
 import NewsLetter from "../newsLetter/newsLetter";
+import {
+  filterProducts,
+  isProductsInLocalStorage,
+  saveProductsToLocalStorage,
+} from "../reusable";
+import useMyCustomFetch from "../customHooks/customFetch";
+import Loader from "../loader/loader";
+import Popup from "../popup/popup";
 
-const Home = () => {
+const HomeDisplay = ({ products }) => {
+  const bestSellers = filterProducts(products, "bestSeller", true);
+  const collectionProducts = filterProducts(products, "bestSeller", false);
   return (
     <>
-      <HomeSlider />
+      <HomeSlider products={bestSellers} />
       <MaterialCare />
-      <Collection />
+      <Collection products={collectionProducts} />
       <NewsLetter />
     </>
   );
+};
+
+const FetchProductsFromLocalStorage = () => {
+  const products = JSON.parse(isProductsInLocalStorage());
+  return <HomeDisplay products={products} />;
+};
+
+const FetchProductsFromServer = () => {
+  const fetchProductState = useMyCustomFetch(
+    "http://localhost:5000/api/shop/products",
+    {
+      products: [],
+    }
+  );
+  const { loading, fetchErr, data } = fetchProductState;
+  if (loading) return <Loader />;
+  if (fetchErr) return <Popup message="can not fetch products" />;
+  const products = data.products;
+  saveProductsToLocalStorage(products);
+  return <HomeDisplay products={products} />;
+};
+
+const Home = () => {
+  const isProductSaved = isProductsInLocalStorage();
+  if (!isProductSaved || JSON.parse(isProductSaved).length === 0)
+    return <FetchProductsFromServer />;
+  return <FetchProductsFromLocalStorage />;
 };
 
 export default Home;
---
+++ src/components/home/home.test.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import Home from "./home";
+import { MemoryRouter } from "react-router-dom";
+import nock from "nock";
+
+describe("testing home components features", () => {
+  const products = [
+    {
+      id: 11,
+      category: "man",
+      bestSeller: true,
+      name: "Graphic PRINT T-SHIRT",
+      description:
+        "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+      price: 40,
+      imageName: "taupe",
+    },
+    {
+      id: 12,
+      category: "man",
+      bestSeller: false,
+      name: "Graphic PRINT T-SHIRT",
+      description:
+        "Round neck T-shirt with short sleeves. Front contrast prints of embroidery.",
+      price: 40,
+      imageName: "black2",
+    },
+    {
+      id: 13,
+      category: "woman",
+      bestSeller: true,
+      name: "VENTED COTTON T-SHIRT",
+      description:
+        "T-shirt made of 100% cotton. Round neck and short sleeves. Side vents at hem.",
+      price: 32,
+      imageName: "ventedBlack",
+    },
+    {
+      id: 14,
+      category: "woman",
+      bestSeller: false,
+      name: "VENTED COTTON T-SHIRT",
+      description:
+        "T-shirt made of 100% cotton. Round neck and short sleeves. Side vents at hem.",
+      price: 32,
+      imageName: "ventedBlue",
+    },
+  ];
+
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test("products are rendered fom localStorage", () => {
+    localStorage.setItem("products", JSON.stringify(products));
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
+    expect(screen.getAllByRole("img")).toHaveLength(5); // includes material care image;
+    expect(screen.getAllByRole("heading")).toHaveLength(6);
+    expect(screen.getByText("Content & Care")).toBeInTheDocument();
+    expect(screen.getByText("Join our newsLetter")).toBeInTheDocument();
+    expect(screen.getByText("Our Collection")).toBeInTheDocument();
+    expect(screen.getByRole("button", "Submit")).toBeInTheDocument();
+  });
+
+  test("products are fetched from the server", async () => {
+    nock("http://localhost:5000")
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/shop/products")
+      .reply(200, { products });
+
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
+
+    expect(await screen.findAllByRole("img")).toHaveLength(5);
+    expect(screen.getAllByRole("heading")).toHaveLength(6);
+    expect(screen.getByText("Content & Care")).toBeInTheDocument();
+    expect(screen.getByText("Join our newsLetter")).toBeInTheDocument();
+    expect(screen.getByText("Our Collection")).toBeInTheDocument();
+    expect(screen.getByRole("button", "Submit")).toBeInTheDocument();
+
+    if (!nock.isDone()) {
+      nock.cleanAll();
+      throw Error("product endpoint was not reached");
+    }
+  });
+});
