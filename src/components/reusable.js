cd8931cb3c837e717fd42cb0bf473e9881a3d3c3
idriss30 <idriss.cissoko@hotmail.com>
2022-09-16 20:57:12 -0400

    new function added (totalPrice, saveCart, totalCount, isCart)

--- src/components/reusable.js
+++ src/components/reusable.js
@@ -9,4 +9,32 @@ const filterProducts = (products, filter, condition) => {
 const isProductsInLocalStorage = () => {
   return localStorage.getItem("products");
 };
-export { saveProductsToLocalStorage, filterProducts, isProductsInLocalStorage };
+
+const saveCartToSessionStorage = (cart) => {
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const isCartInSessionStorage = () => {
+  return sessionStorage.getItem("cart");
+};
+
+const totalCountQuantity = (cart) => {
+  return cart.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
+};
+
+const totalPrice = (cart) => {
+  return cart.reduce((init, product) => {
+    return init + product.price * product.quantity;
+  }, 0);
+};
+export {
+  saveProductsToLocalStorage,
+  filterProducts,
+  isProductsInLocalStorage,
+  saveCartToSessionStorage,
+  totalCountQuantity,
+  isCartInSessionStorage,
+  totalPrice,
+};
