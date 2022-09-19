bd7d86dfaa9d9b4712ef9fa80dac8d412fcbdf9f
idriss30 <idriss.cissoko@hotmail.com>
2022-09-18 23:08:47 -0400

    store refactored into a function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { setupTheStore } from "./redux/store";
 import "swiper/css/bundle";
 import "./index.css";
 import App from "./App";
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider store={setupTheStore()}>
         <App />
       </Provider>
     </BrowserRouter>
