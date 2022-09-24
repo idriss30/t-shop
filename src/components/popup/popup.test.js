ffd1bfef1082740bdca6a3e9bf89035309d5cffd
idriss30 <idriss.cissoko@hotmail.com>
2022-09-24 04:33:24 -0400

    replaced test label and changed button assertion

--- src/components/popup/popup.test.js
+++ src/components/popup/popup.test.js
@@ -8,10 +8,10 @@ test("render popup Content", async () => {
   render(<Popup message={"testing rendering"} />);
 
   expect(screen.getByText(/testing/i)).toBeInTheDocument();
-  expect(screen.getByRole("button", "X")).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "X" })).toBeInTheDocument();
 });
 
-test("wait 2 sec before closing the popup", () => {
+test("should wait 2 sec before closing the popup", () => {
   render(<Popup message={"fade"} />);
   expect(setTimeout).toHaveBeenCalledTimes(1);
   expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
