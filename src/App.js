15af8d6e1422576523e8401aa0245a70857e8486
idriss30 <idriss.cissoko@hotmail.com>
2022-11-15 16:11:41 -0500

    adding new routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/** @jsxImportSource @emotion/react */
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Home from "./components/home/home";
@@ -6,18 +5,23 @@ import Gender from "./components/GenderComponent/genderComponent";
 import { NavBar } from "./components/navBar/navBar";
 import SingleProduct from "./components/singleProduct/singleProduct";
 import { Cart } from "./components/cart/cart";
+import PageDisplay from "./components/login-register/login";
+import Register from "./components/login-register/register";
+import Profile from "./components/profile/profile";
 
 function App() {
   return (
     <div className="App">
       <NavBar counter={0} />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/man" element={<Gender />} />
         <Route path="/woman" element={<Gender />}></Route>
         <Route path="/shop/:productName" element={<SingleProduct />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/login" element={<PageDisplay />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/users/profile" element={<Profile />}></Route>
       </Routes>
       <Footer />
     </div>
