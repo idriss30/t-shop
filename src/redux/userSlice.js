f147d891da501edb5cffd467aa4af6822d0a98d5
idriss30 <idriss.cissoko@hotmail.com>
2022-11-16 19:23:28 -0500

    user slice updated to get user, log user out and delete user

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,9 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const calculateOneHour = () => new Date(1 * 3600 * 1000);
 const writeCookie = (username) =>
-  document.cookie(`user=${username}; expires = ${calculateOneHour()}`);
+  (document.cookie = `user=${username}; max-age=3600; path=/`);
 const checkUserCookie = () => {
   const cookiesNameArray = document.cookie.split("=");
   const isCookiePresent = cookiesNameArray.find(
@@ -13,12 +12,21 @@ const checkUserCookie = () => {
   return false;
 };
 
+const clearCookie = () => {
+  document.cookie = `user=""; max-age=0; path=/`;
+  const isCookiePresent = checkUserCookie();
+  if (isCookiePresent) {
+    document.cookie = `user=""; max-age=0; path=/`;
+  }
+  return true;
+};
+
 const initialState = {
   isLoading: false,
   isLoggedIn: checkUserCookie(),
-  userInfo: {},
-  error: false,
+  error: null,
   isItFullFilled: false,
+  isDeleted: false,
 };
 
 export const getUser = createAsyncThunk("user/login", async (user) => {
@@ -29,34 +37,98 @@ export const getUser = createAsyncThunk("user/login", async (user) => {
   );
   return loginResponse.data;
 });
+export const logoutUser = createAsyncThunk("/user/logout", async () => {
+  const signoutResponse = await axios.get(
+    `${process.env.REACT_APP_URL}/api/users/signout`,
+    { withCredentials: true }
+  );
+  if (signoutResponse.status === 200) {
+    clearCookie();
+  }
+  return signoutResponse.data;
+});
+
+export const deleteUser = createAsyncThunk("/user/delete", async (username) => {
+  const deleteRequest = await axios.delete(
+    `${process.env.REACT_APP_URL}/api/users/delete/${username}`,
+    { withCredentials: true }
+  );
+
+  return deleteRequest.data;
+});
+
+const reset = (state) => {
+  clearCookie();
+  state.isItFullFilled = false;
+  state.isLoggedIn = checkUserCookie();
+  state.error = null;
+  state.isLoading = false;
+  state.isDeleted = false;
+};
+
+const resetDeleteUserError = (state) => {
+  state.error = null;
+};
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    update: () => null,
-    logout: () => null,
-    deleteUser: () => null,
+    resetState: reset,
+    resetUserError: resetDeleteUserError,
   },
   extraReducers(builder) {
     builder
       .addCase(getUser.pending, (state) => {
         state.isLoading = true;
-        state.error = false;
+        state.error = null;
         state.isItFullFilled = false;
       })
       .addCase(getUser.fulfilled, (state, action) => {
         writeCookie(action.payload.user.username);
         state.isLoading = false;
         state.isItFullFilled = true;
-        state.userInfo = { ...action.payload.user };
-        state.error = false;
+        state.isLoggedIn = true;
+        state.isDeleted = false;
       })
-      .addCase(getUser.rejected, (state) => {
-        state.error = true;
+      .addCase(getUser.rejected, (state, action) => {
+        state.error = action.error.message;
         state.isLoading = false;
         state.isItFullFilled = false;
+        state.isLoggedIn = checkUserCookie();
+        state.isDeleted = false;
       });
+
+    builder.addCase(logoutUser.fulfilled, (state) => {
+      clearCookie();
+      state.isLoading = false;
+      state.error = null;
+      state.isItFullFilled = true;
+      state.isLoggedIn = checkUserCookie();
+      state.isDeleted = false;
+    });
+    builder.addCase(logoutUser.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
+      state.isItFullFilled = false;
+      state.isLoggedIn = checkUserCookie();
+      state.isDeleted = false;
+    });
+    builder.addCase(deleteUser.fulfilled, (state) => {
+      clearCookie();
+      state.isItFullFilled = true;
+      state.isLoading = false;
+      state.error = null;
+      state.isLoggedIn = false;
+      state.isDeleted = true;
+    });
+    builder.addCase(deleteUser.rejected, (state, action) => {
+      state.isItFullFilled = false;
+      state.isLoading = false;
+      state.error = action.error.message;
+      state.isLoggedIn = checkUserCookie();
+      state.isDeleted = false;
+    });
   },
 });
-export const { update, logout, deleteUser } = userSlice.actions;
+export const { resetState, resetUserError } = userSlice.actions;
 export default userSlice.reducer;
