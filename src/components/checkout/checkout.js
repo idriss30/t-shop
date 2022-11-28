023efdf582e14423e884aa83508a459ab233dc09
idriss30 <idriss.cissoko@hotmail.com>
2022-11-27 20:30:35 -0500

    style update

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -16,6 +16,13 @@ const sectionStyle = {
   "@media(max-width:620px)": {
     width: "75%",
   },
+  h1: {
+    fontSize: "1.1rem",
+    padding: "1rem 0 .5rem 0",
+  },
+  a: {
+    color: "red",
+  },
 };
 
 const InviteToLogin = () => {
