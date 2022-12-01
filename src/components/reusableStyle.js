76b2f1bf8600674be59062e0f77128a71d789ee9
idriss30 <idriss.cissoko@hotmail.com>
2022-12-01 15:54:35 -0500

    refactoring style to facilitate Dry principle

---
+++ src/components/reusableStyle.js
@@ -0,0 +1,17 @@
+const sectionStyle = {
+  width: "70%",
+  minHeight: "60vh",
+  margin: "6rem auto",
+  "@media(max-width:620px)": {
+    width: "75%",
+  },
+  h1: {
+    fontSize: "1.1rem",
+    padding: "1rem 0 .5rem 0",
+  },
+  a: {
+    color: "red",
+  },
+};
+
+export { sectionStyle };
