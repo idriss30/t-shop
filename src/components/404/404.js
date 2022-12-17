b19ac37a54a910f0132df7156ed9ba9867e8ebbc
idriss30 <idriss.cissoko@hotmail.com>
2022-12-17 18:32:56 -0500

    404 page added

---
+++ src/components/404/404.js
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { sectionStyle } from "../reusableStyle";
+
+const imgStyle = {
+  width: "30vw",
+  height: "50vh",
+  display: "block",
+  margin: "0 auto",
+
+  "@media(max-width:520px)": {
+    width: "auto",
+    height: "auto",
+  },
+};
+
+const Render404 = () => {
+  return (
+    <section css={sectionStyle}>
+      <h1>Nothing to see here, Please navigate away.</h1>
+      <p>Some pages were left empty because this is not a commercial shop</p>
+      <img
+        css={imgStyle}
+        alt="404"
+        src={`${require("../../assets/404.png")}`}
+      />
+    </section>
+  );
+};
+
+export default Render404;
