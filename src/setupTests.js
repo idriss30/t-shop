83d5996e44a661ac9f4af87389cfecd5a8371c0a
idriss30 <idriss.cissoko@hotmail.com>
2022-12-20 17:33:32 -0500

    mocking interSection observer Api

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+const mockIntersectionObserver = jest.fn();
+
+mockIntersectionObserver.mockReturnValue({
+  observe: () => null,
+  unobserve: () => null,
+});
+window.IntersectionObserver = mockIntersectionObserver;
