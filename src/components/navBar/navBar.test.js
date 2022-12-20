0852ca415562380c2f24fca3c0f9e03a8dcdade9
idriss30 <idriss.cissoko@hotmail.com>
2022-12-20 17:49:34 -0500

    test refactor

--- src/components/navBar/navBar.test.js
+++ src/components/navBar/navBar.test.js
@@ -15,9 +15,9 @@ describe("checking navbar links presence and features", () => {
       </MemoryRouter>
     );
     expect(screen.getByText("TSHOP")).toBeInTheDocument();
-    expect(screen.getByText("Man")).toBeInTheDocument();
-    expect(screen.getByText("Woman")).toBeInTheDocument();
-    expect(screen.getByText("Log in")).toBeInTheDocument();
+    expect(screen.getByText(/Man/)).toBeInTheDocument();
+    expect(screen.getByText(/Woman/)).toBeInTheDocument();
+    expect(screen.getByText(/log in/i)).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveAttribute("src", "cart.svg");
   });
 });
