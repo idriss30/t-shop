c041913185f9d866d7e9ca7ee88b7aab9c4e4697
idriss30 <idriss.cissoko@hotmail.com>
2022-12-24 19:22:23 -0500

    created test for not successful login

--- src/components/login-register/login.test.js
+++ src/components/login-register/login.test.js
@@ -1,9 +1,16 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { renderWithProviders } from "../../redux/testUtils";
+import nock from "nock";
 import PageDisplay from "./login";
 
 describe("testing login features", () => {
+  beforeEach(() => {
+    if (!nock.isDone()) {
+      throw new Error("some endpoints were not reached");
+    }
+    nock.cleanAll();
+  });
   test("should render login component", () => {
     renderWithProviders(
       <BrowserRouter>
@@ -29,5 +36,60 @@ describe("testing login features", () => {
     expect(passwordInput).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(registerLink).toBeInTheDocument();
+  });
+
+  test("can display error server error on wrong login attempt", async () => {
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .post("/api/users/login")
+      .reply(400);
+
+    renderWithProviders(
+      <BrowserRouter>
+        <PageDisplay />
+      </BrowserRouter>
+    );
+
+    const usernameInput = screen.getByPlaceholderText("username");
+    fireEvent.change(usernameInput, { target: { value: "john" } });
+    const passwordInput = screen.getByPlaceholderText("password");
+    fireEvent.change(passwordInput, { target: { value: "wrongPassword" } });
+    const form = screen.getByTitle("form");
+    fireEvent.submit(form);
+
+    const popupErrorText = screen.findByText(
+      /request failed with status code 400/i
+    );
+    expect(await popupErrorText).toBeInTheDocument();
+  });
+
+  test("can handle successful login", async () => {
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .post("/api/users/login")
+      .reply(200, { message: "log in you in" });
+
+    renderWithProviders(
+      <BrowserRouter>
+        <PageDisplay />
+      </BrowserRouter>
+    );
+    const successInputName = screen.getByPlaceholderText("username");
+    fireEvent.change(successInputName, { target: { value: "validUser" } });
+    const successInputPassword = screen.getByPlaceholderText("password");
+    fireEvent.change(successInputPassword, {
+      target: { value: "goodPassword" },
+    });
+
+    const submitButton = screen.getByRole("button", { name: /log in/i });
+    fireEvent.click(submitButton);
+
+    expect("");
   });
 });
