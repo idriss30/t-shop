87241c8827efda73c6f1933b3927c96b464db480
idriss30 <idriss.cissoko@hotmail.com>
2023-01-01 16:00:00 -0500

    adding test cases to profile.test.js

--- src/components/profile/profile.test.js
+++ src/components/profile/profile.test.js
@@ -1,28 +1,62 @@
-import { screen, fireEvent } from "@testing-library/react";
+import {
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import nock from "nock";
 import { renderWithProviders } from "../../redux/testUtils";
 import Profile from "./profile";
+import { deleteUser, logoutUser } from "../../redux/userSlice";
 
-describe("testing profile features", () => {
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+let userInfo = {
+  username: "test",
+  firstname: "test",
+  lastname: "tester",
+  email: "test@email.com",
+  password: "test",
+  address: "123 my apt",
+  city: "boston",
+  state: "massachusetts",
+  zip: 12345,
+};
+
+let initialLoggedInState = {
+  isLoading: false,
+  isLoggedIn: true,
+  error: null,
+  userInfo,
+  isItFullFilled: false,
+};
+
+const globalState = {
+  cart: { totalProducts: 0, products: [] },
+  user: {
+    isLoading: false,
+    isLoggedIn: true,
+    error: null,
+    userInfo,
+    isItFullFilled: false,
+  },
+};
+describe("can handle successfull request", () => {
   afterEach(() => {
     if (!nock.isDone()) {
       nock.cleanAll();
-      throw Error("some endpoints not reached! check config");
+      throw new Error("some endpoints were not reached");
     }
   });
-
-  const user = {
-    username: "test",
-    firstname: "test",
-    lastname: "tester",
-    email: "test@email.com",
-    password: "test",
-    address: "123 my apt",
-    city: "boston",
-    state: "massachusetts",
-    zip: 12345,
-  };
+  beforeEach(() => {
+    jest.runAllTimers();
+  });
 
   test("successfull user profile rendering", async () => {
     nock(`${process.env.REACT_APP_URL}`)
@@ -31,25 +65,29 @@ describe("testing profile features", () => {
         "access-control-allow-credentials": "true",
       })
       .get("/api/users/profile")
-      .reply(200, { user });
+      .reply(200, { user: userInfo });
 
     renderWithProviders(
       <BrowserRouter>
         <Profile />
-      </BrowserRouter>
+      </BrowserRouter>,
+      { preloadedState: { user: initialLoggedInState } }
     );
+
     expect(screen.getByText(/account information/i)).toBeInTheDocument();
-    const greetingsUser = `Welcome ${user.firstname},`;
+    const greetingsUser = `Welcome ${userInfo.firstname},`;
     expect(await screen.findByText(greetingsUser)).toBeInTheDocument();
     expect(
       screen.getByText(/here is what we have on you/i)
     ).toBeInTheDocument();
-    expect(screen.getByText(user.email)).toBeInTheDocument();
+
+    expect(screen.getByText(userInfo.email)).toBeInTheDocument();
     expect(
       screen.getByText(
-        `${user.address}, ${user.city} ${user.state} ${user.zip}`
+        `${userInfo.address}, ${userInfo.city} ${userInfo.state} ${userInfo.zip}`
       )
     ).toBeInTheDocument();
+
     expect(screen.getAllByRole("link")).toHaveLength(4);
   });
 
@@ -60,20 +98,162 @@ describe("testing profile features", () => {
         "access-control-allow-credentials": "true",
       })
       .get("/api/users/profile")
-      .reply(200, { user });
+      .reply(200, { user: userInfo });
+
+    renderWithProviders(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+
+      { preloadedState: { user: initialLoggedInState } }
+    );
+
+    // wait for endpoint to be reached (async request)
+    const greetingsUser = `Welcome ${userInfo.firstname},`;
+    expect(await screen.findByText(greetingsUser)).toBeInTheDocument();
+    // get update button and assert on it
+    const updateButton = screen.getByRole("link", { name: /update/i });
+    fireEvent.click(updateButton);
+    expect(window.location.pathname).toBe("/users/update");
+  });
+
+  test("should successfully log out", async () => {
+    // successfull nock rendering
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/users/profile")
+      .reply(200, { user: userInfo });
+
+    //mocking signout response for user
+
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/users/signout")
+      .reply(200, { message: "success" });
+
+    renderWithProviders(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+      { preloadedState: { user: initialLoggedInState } }
+    );
+
+    expect(await screen.findByText("Welcome test,")).toBeInTheDocument();
+
+    const logoutButton = screen.getByRole("link", { name: /logout/i });
+
+    fireEvent.click(logoutButton);
+    expect(
+      await screen.findByText("you are being logged out")
+    ).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("you are being logged out")
+    );
+
+    // check redirection
+    expect(window.location.pathname).toBe("/");
+  });
+
+  test("can successfully delete user", async () => {
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/users/profile")
+      .reply(200, { user: userInfo });
+
+    /*browsers send an OPTIONS request before a DELETE request which return a no match response
+    one option to solve this issue would be to run intercept with nock before the delete or 
+    add setupEnvironment = node to your test config.
+    I choose the first to avoid dealing with ejecting create-react-app
+     */
+
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .intercept("/api/users/delete/test", "OPTIONS")
+      .reply(200, null, {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application:json",
+      })
+      .delete("/api/users/delete/test")
+      .reply(200, { message: "success" });
+
+    const { store } = renderWithProviders(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+      { preloadedState: { user: initialLoggedInState } }
+    );
+
+    const welcome = screen.findByText(/welcome test,/i);
+    expect(await welcome).toBeInTheDocument();
+    expect(store.getState()).toEqual(globalState);
+    const deleteButton = screen.getByRole("link", { name: /delete/i });
+    fireEvent.click(deleteButton);
+    expect(
+      await screen.findByText("your account has been deleted")
+    ).toBeInTheDocument();
+
+    expect(window.location.pathname).toBe("/");
+  });
+});
+
+describe("can handle wrong requests", () => {
+  beforeEach(() => {
+    if (!nock.isDone()) {
+      nock.cleanAll();
+      throw Error("some endpoints were not reached in error describe");
+    }
+  });
+
+  beforeEach(() => {
+    jest.runAllTimers();
+  });
+
+  test("should handle wrong token Error", async () => {
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/users/profile")
+      .reply(403);
 
     renderWithProviders(
       <BrowserRouter>
         <Profile />
       </BrowserRouter>
     );
-    // wait for endpoint to be reached (async request)
-    const greetingsUser = `Welcome ${user.firstname},`;
-    expect(await screen.findByText(greetingsUser)).toBeInTheDocument();
 
-    // get update button and assert on it
-    const updateButton = screen.getByRole("link", { name: /update/i });
-    fireEvent.click(updateButton);
-    expect(window.location.pathname).toEqual("/users/update");
+    expect(await screen.findByText("token not valid")).toBeInTheDocument();
+  });
+
+  test("should handle connection error", async () => {
+    nock(`${process.env.REACT_APP_URL}`)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/api/users/profile")
+      .reply(400);
+
+    renderWithProviders(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>
+    );
+
+    expect(await screen.findByText("network error")).toBeInTheDocument();
   });
 });
