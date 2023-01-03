655bb3e2fa407f2b808fab1fd145a72fe8b53bd5
idriss30 <idriss.cissoko@hotmail.com>
2023-01-03 13:52:02 -0500

    refactored entire logic for order.js

--- src/components/profile/order.js
+++ src/components/profile/order.js
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import useMyCustomFetch from "../customHooks/customFetch";
 import Loader from "../loader/loader";
@@ -50,105 +50,97 @@ const orderInfoStyle = {
   },
 };
 
-const NoOrders = ({ firstName }) => {
+const NoOrders = ({ firstName, hideOrders }) => {
+  useLayoutEffect(() => {
+    sectionFading();
+  }, []);
   return (
-    <div css={noOrderStyle}>
-      <p>No orders found for your account {firstName}.</p>
-      <p>Please visit the store and get you some nice tee-shirts !</p>
-    </div>
+    <section aria-label="order-section" css={sectionStyle}>
+      <h1>Orders details</h1>
+      <Link to={"#"} onClick={hideOrders}>
+        <span> &larr; Go back</span>
+      </Link>
+      <div css={noOrderStyle}>
+        <p>No orders found for your account {firstName}.</p>
+        <p>Please visit the store and get you some nice tee-shirts !</p>
+      </div>
+    </section>
   );
 };
 
-const RenderOrders = ({ orders }) => {
+const RenderOrders = ({ orders, hideOrders }) => {
+  useLayoutEffect(() => {
+    sectionFading();
+  }, []);
   return (
-    <div css={renderOrdersStyle}>
-      <p>
-        we have <span>{orders.length}</span> order(s) on you
-      </p>
-      <ul>
-        {orders.map((order) => {
-          return (
-            <li key={order.id}>
-              <p>
-                One order created :
-                {new Date(`${order.createdAt}`).toLocaleString()} for a total of
-                <span> ${order.totalOrder}</span>
-              </p>
-              <div>
+    <section aria-label="order-section" css={sectionStyle}>
+      <h1>Orders details</h1>
+      <Link to={"#"} onClick={hideOrders}>
+        <span> &larr; Go back</span>
+      </Link>
+      <div css={renderOrdersStyle}>
+        <p>
+          we have <span>{orders.length}</span> order(s) on you
+        </p>
+        <ul>
+          {orders.map((order) => {
+            return (
+              <li key={order.id}>
                 <p>
-                  To be delivered at:
-                  <span>{` ${order.address} ${order.city} ${order.state} ${order.zip}`}</span>
+                  One order created :
+                  {new Date(`${order.createdAt}`).toLocaleString()} for a total
+                  of
+                  <span> ${order.totalOrder}</span>
                 </p>
-                <p>Item(s) for this order:</p>
-                <p>Click image picture to see product</p>
-              </div>
-              <div>
-                {Object.values(JSON.parse(order.items)).map((item) => {
-                  return (
-                    <div key={order.id + Math.random()} css={orderInfoStyle}>
-                      <p>
-                        {` ${item.qty} ${item.size} ${item.name.toLowerCase()}`}
-                      </p>
-                      <Link to={`/shop/${item.img}`}>
-                        <img
-                          src={require(`../../assets/${item.img}-display.jpg`)}
-                          alt={item.name}
-                        />
-                      </Link>
-                    </div>
-                  );
-                })}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                <div>
+                  <p>
+                    To be delivered at:
+                    <span>{` ${order.address} ${order.city} ${order.state} ${order.zip}`}</span>
+                  </p>
+                  <p>Item(s) for this order:</p>
+                  <p>Click image picture to see product</p>
+                </div>
+                <div>
+                  {Object.values(JSON.parse(order.items)).map((item) => {
+                    return (
+                      <div key={order.id + Math.random()} css={orderInfoStyle}>
+                        <p>
+                          {` ${item.qty} ${
+                            item.size
+                          } ${item.name.toLowerCase()}`}
+                        </p>
+                        <Link to={`/shop/${item.img}`}>
+                          <img
+                            src={require(`../../assets/${item.img}-display.jpg`)}
+                            alt={item.name}
+                          />
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
   );
 };
 
 const Orders = ({ firstName, hideOrders }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [content, setContent] = useState("");
-
-  useLayoutEffect(() => {
-    sectionFading();
-  }, []);
   const orderState = useMyCustomFetch(
     `${process.env.REACT_APP_URL}/api/cart/orders/${firstName}`,
     { orders: [] }
   );
   const { loading, fetchErr, data } = orderState;
+
   // const ordersInfo = data.orders;
-  useEffect(() => {
-    if (loading) {
-      setIsLoading(true);
-    }
-    if (fetchErr) {
-      setIsLoading(false);
-      setContent(<NoOrders firstName={firstName} />);
-    }
-    if (data) {
-      setIsLoading(false);
-      if (data.orders.length === 0)
-        return setContent(<NoOrders firstName={firstName} />);
-      setContent(<RenderOrders orders={data.orders} />);
-    }
-  }, [loading, fetchErr, data, firstName]);
-
-  return (
-    <>
-      {isLoading && <Loader />}
-
-      <section css={sectionStyle}>
-        <h1>Orders details</h1>
-        <Link to={"#"} onClick={hideOrders}>
-          <span> &larr; Go back</span>
-        </Link>
-        {content}
-      </section>
-    </>
-  );
+  if (loading) return <Loader />;
+  if (fetchErr)
+    return <NoOrders firstName={firstName} hideOrders={hideOrders} />;
+  if (data && data.orders.length > 0)
+    return <RenderOrders orders={data.orders} hideOrders={hideOrders} />;
 };
 
 export default Orders;
