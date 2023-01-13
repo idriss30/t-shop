eb552463caa66daef30a45ee71b9359dee7c852c
idriss30 <idriss.cissoko@hotmail.com>
2023-01-13 05:26:24 -0500

    adding more test cases to checkout page

--- src/components/checkout/checkout.test.js
+++ src/components/checkout/checkout.test.js
@@ -1,4 +1,4 @@
-import { screen, fireEvent } from "@testing-library/react";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 import { renderWithProviders } from "../../redux/testUtils";
 import { BrowserRouter } from "react-router-dom";
 import nock from "nock";
@@ -20,7 +20,7 @@ let initialUserLoggedInState = {
   isLoading: false,
   isLoggedIn: true,
   error: null,
-  userInfo,
+  userInfo: {},
   isItFullFilled: false,
 };
 
@@ -54,7 +54,25 @@ const cartState = {
   ],
 };
 
+beforeEach(() => {
+  if (!nock.isDone()) {
+    nock.cleanAll();
+    throw Error("some endpoints were not reached");
+  }
+});
 test("can render checkout form without user", async () => {
+  //mocking payment intent request to server
+  nock(`${process.env.REACT_APP_URL}`)
+    .defaultReplyHeaders({
+      "access-control-allow-origin": "*",
+      "access-control-allow-credentials": "true",
+    })
+    .post("/api/stripe/paymentIntent")
+    .reply(201, {
+      clientSecret:
+        "pi_1Dstq32eZvKYlo2CShnP61Sg_secret_FAuC6DTX3Tnpfm9E7E6fg0HAb",
+    });
+
   //not checking stripe rendering
   renderWithProviders(
     <BrowserRouter>
@@ -79,10 +97,61 @@ test("can render checkout form without user", async () => {
   expect(screen.getByPlaceholderText(/city/i)).toBeInTheDocument();
   expect(screen.getByPlaceholderText(/state/i)).toBeInTheDocument();
   expect(screen.getByPlaceholderText(/zip/i)).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("button", { name: /place order/i })
+  ).toBeInTheDocument();
 });
 
-test("can render checkout form with user info", () => {
+test("can render checkout form with user info", async () => {
+  //mocking fetch userInfo details
+  nock(`${process.env.REACT_APP_URL}`)
+    .defaultReplyHeaders({
+      "access-control-allow-origin": "*",
+      "access-control-allow-credentials": "true",
+    })
+    .get("/api/users/profile")
+    .reply(200, { user: userInfo });
+
+  //mocking stripe paymentIntent request
+  nock(`${process.env.REACT_APP_URL}`)
+    .defaultReplyHeaders({
+      "access-control-allow-origin": "*",
+      "access-control-allow-credentials": "true",
+    })
+    .post("/api/stripe/paymentIntent")
+    .reply(201, {
+      clientSecret: nock(`${process.env.REACT_APP_URL}`).defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      }),
+    });
+
   //not checking stripe rendering
+  renderWithProviders(
+    <BrowserRouter>
+      <Checkout />
+    </BrowserRouter>,
+    {
+      preloadedState: {
+        cart: cartState,
+        user: initialUserLoggedInState,
+      },
+    }
+  );
+
+  expect(
+    await screen.findByText(`Hello, ${userInfo.firstname}`)
+  ).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.lastname)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.email)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.address)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.city)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.state)).toBeInTheDocument();
+  expect(screen.getByDisplayValue(userInfo.zip)).toBeInTheDocument();
+  expect(
+    screen.getByRole("button", { name: /place order/i })
+  ).toBeInTheDocument();
 });
 
 test("can handle successful stripe checkout", () => {});
