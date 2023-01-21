6d580a75eb34438007b746dba692f39b0e0cdc5e
idriss30 <idriss.cissoko@hotmail.com>
2023-01-21 02:45:51 -0500

    removing most of the logic from Checkout component and putting it in CheckoutForm

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -1,13 +1,10 @@
 /** @jsxImportSource @emotion/react */
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { myUseSelector } from "../../redux/reduxHooks";
-import { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./checkoutForm";
-import axios from "axios";
-import Popup from "../popup/popup";
-import Loader from "../loader/loader";
+
 import { sectionStyle } from "../reusableStyle";
 const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE}`);
 
@@ -25,52 +22,12 @@ const InviteToLogin = () => {
 
 const Checkout = () => {
   const isLoggedIn = myUseSelector((state) => state.user.isLoggedIn);
-
-  const products = myUseSelector((state) => state.cart.products);
-  const [popup, setPopup] = useState(false);
-  const [loading, setIsLoading] = useState(true);
-  const [clientSecret, setClientSecret] = useState("");
-  const [tokenError, setTokenError] = useState(false);
-  const [popupMessage, setPopupMessage] = useState();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      const response = await axios.post(
-        `${process.env.REACT_APP_URL}/api/stripe/paymentIntent`,
-        { products }
-      );
-      return response.data;
-    };
-    fetchToken()
-      .then((response) => {
-        setIsLoading(false);
-        setClientSecret(response.clientSecret);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        setPopupMessage(error.message);
-        setPopup(true);
-        setTokenError(true);
-      });
-  }, [products]);
-
-  useEffect(() => {
-    if (products.length === 0 || tokenError) {
-      setTimeout(() => {
-        navigate("/");
-      }, 1000);
-    }
-  }, [navigate, products.length, tokenError]);
-
   return (
     <>
-      {loading && <Loader />}
-      {popup && <Popup message={popupMessage} remove={() => setPopup(false)} />}
       <section css={sectionStyle}>
         {!isLoggedIn && <InviteToLogin />}
         <Elements stripe={stripePromise}>
-          <CheckoutForm clientSecret={clientSecret} />
+          <CheckoutForm />
         </Elements>
       </section>
     </>
